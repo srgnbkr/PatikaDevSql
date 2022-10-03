b4653c0fede343b5b00ea20c5fa46b7e85f989ca
Sergen Bakır <cuseg09@gmail.com>
2022-10-03 17:02:50 +0300

    Hw5 Added

---
+++ Hw5.sql
@@ -0,0 +1,13 @@
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+select * from film
+where title like '%n'
+order by  length desc  limit 5;
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+select * from film
+where title like '%n'
+order by length  limit 5 offset 5;
+--customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+select last_name from customer
+where store_id=1
+order by last_name limit 4;
+
